b06b9b9e016c30a99e1ee4eb5f0f7f4dccd3ea28
MSJOAN02 <joan@jmcode.dev>
2024-01-10 19:36:35 +0100

    Siete y medio web

--- Sandbox-TS copy/typescript-sandbox/src/main.ts
+++ Sandbox-TS copy/typescript-sandbox/src/main.ts
@@ -1,18 +1,14 @@
+// TODO - Botón nueva partida
+
 import "./style.css";
 
 let puntuacion: number = 0;
 
-function gestionarGameOver(puntos: number): boolean {
-  if (puntos < 7.5) {
-    return false;
-  } else if (puntos > 7.5) {
-    return true;
-  } else {
-    return false;
-  }
-}
+type Estado = "GAME_OVER" | "PUEDE_CONTINUAR" | "ME_PLANTO";
 
-function muestraPuntuacion(puntos: number): void {
+// Función para mostrar la puntuación en todo momento
+
+function muestraPuntuacion(puntos: number): Estado {
   const handlePuntuacion = document.getElementById("puntuacionTotal");
   if (
     handlePuntuacion !== null &&
@@ -21,18 +17,34 @@ function muestraPuntuacion(puntos: number): void {
   ) {
     handlePuntuacion.innerHTML = `Puntuación actual: ${puntos}`;
   }
+
+  if (puntos < 7.5) {
+    return "PUEDE_CONTINUAR";
+  } else if (puntos > 7.5) {
+    return "GAME_OVER";
+  } else {
+    return "PUEDE_CONTINUAR";
+  }
 }
 
-function pideCarta(): void {
+/* Función para pedir carta.
+    Genera un número aleatorio hasta que este no sea ni 8 ni 9.
+    Encadena con la funcion de mostrar carta.
+*/
+
+function pideCarta(): number {
   let numero: number;
   do {
     numero = Math.floor(Math.random() * 12) + 1;
   } while (numero === 8 || numero === 9);
-
-  mostrarCarta(numero);
+  return numero;
 }
 
-function mostrarCarta(numeroCarta: number): void {
+/* Función para mostrar carta
+    Recibe el número de carta
+*/
+
+function mostrarCarta(numeroCarta: number): number {
   const nuevaCarta = document.createElement("img");
   nuevaCarta.classList.add("miMazo");
   let recurso: string = "";
@@ -78,25 +90,103 @@ function mostrarCarta(numeroCarta: number): void {
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
       break;
   }
-  if (numeroCarta <= 12 && numeroCarta >= 10) {
-    puntuacion = puntuacion + 0.5;
-    muestraPuntuacion(puntuacion);
-  } else {
-    puntuacion = puntuacion + numeroCarta;
-    muestraPuntuacion(puntuacion);
-  }
   nuevaCarta.src = recurso;
   const misCartas = document.getElementById("misCartas");
 
   if (misCartas && misCartas instanceof HTMLDivElement) {
     misCartas.appendChild(nuevaCarta);
   }
+  if (numeroCarta <= 12 && numeroCarta >= 10) {
+    return 0.5;
+  } else {
+    return numeroCarta;
+  }
 }
-const handlePedirCarta = document.getElementById("pedirCarta");
 
-if (handlePedirCarta && handlePedirCarta instanceof HTMLButtonElement) {
-  handlePedirCarta.addEventListener("click", pideCarta);
+function muestraMensaje(mensaje: string): void {
+  const divMensaje = document.getElementById("mensaje");
+  if (
+    divMensaje !== null &&
+    divMensaje !== undefined &&
+    divMensaje instanceof HTMLDivElement
+  ) {
+    divMensaje.innerHTML = mensaje;
+  }
 }
+
+function gestionarGameOver(estado: Estado, puntosTotal: number): string {
+  const elementoPedir = document.getElementById("pedirCarta");
+  const elementoPlanto = document.getElementById("mePlanto");
+  let mensaje = "";
+  if (
+    elementoPedir &&
+    elementoPedir instanceof HTMLButtonElement &&
+    elementoPlanto &&
+    elementoPlanto instanceof HTMLButtonElement
+  ) {
+    if (estado === "GAME_OVER") {
+      elementoPedir.disabled = true;
+      elementoPlanto.disabled = true;
+      mensaje =
+        'Vaya! Parece que te has pasado. Pulsa "Nueva Partida" para volver a probar suerte';
+    } else if (estado === "ME_PLANTO") {
+      elementoPedir.disabled = true;
+      elementoPlanto.disabled = true;
+      if (puntosTotal < 4) {
+        mensaje = "Has sido muy conservador.";
+      } else if (puntosTotal === 5) {
+        mensaje = "Te ha entrado el canguelo eh?";
+      } else if (puntosTotal === 6 || puntosTotal === 7) {
+        mensaje = "Casi casi...";
+      } else if (puntosTotal === 7.5) {
+        mensaje = "¡Lo has clavado! ¡Enhorabuena!";
+      } else {
+        mensaje = "No se que ha pasado pero no deberíamos estar aquí.";
+      }
+    }
+  }
+
+  return mensaje;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   muestraPuntuacion(puntuacion);
 });
+
+const handleCompruebaClick = () => {
+  puntuacion = puntuacion + mostrarCarta(pideCarta());
+  const estado: Estado = muestraPuntuacion(puntuacion);
+  const mensajeMuestra = gestionarGameOver(estado, puntuacion);
+  const handleMensaje = document.getElementById("mensaje");
+
+  if (
+    handleMensaje !== null &&
+    handleMensaje !== undefined &&
+    handleMensaje instanceof HTMLDivElement
+  ) {
+    handleMensaje.innerHTML = mensajeMuestra;
+  }
+};
+
+const handlePlantar = document.getElementById("mePlanto");
+const handleClickPedir = document.getElementById("pedirCarta");
+
+if (
+  handleClickPedir &&
+  handleClickPedir instanceof HTMLButtonElement &&
+  handlePlantar &&
+  handlePlantar instanceof HTMLButtonElement
+) {
+  handleClickPedir.addEventListener("click", handleCompruebaClick);
+  handlePlantar.addEventListener("click", () => {
+    const mensajeMuestra = gestionarGameOver("ME_PLANTO", puntuacion);
+    const handleMensaje = document.getElementById("mensaje");
+    if (
+      handleMensaje !== null &&
+      handleMensaje !== undefined &&
+      handleMensaje instanceof HTMLDivElement
+    ) {
+      handleMensaje.innerHTML = mensajeMuestra;
+    }
+  });
+}
