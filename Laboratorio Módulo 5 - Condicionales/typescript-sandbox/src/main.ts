a5a0699c3841f57e43555f4c56bc34fbac2132d5
MSJOAN02 <joan@jmcode.dev>
2024-01-15 17:55:53 +0100

    Correcciones varias

--- Laboratorio Módulo 5 - Condicionales/typescript-sandbox/src/main.ts
+++ Laboratorio Módulo 5 - Condicionales/typescript-sandbox/src/main.ts
@@ -26,37 +26,36 @@ const generarUrlCarta = (numeroCarta: number): string => {
   switch (numeroCarta) {
     case 1:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
-      break;
+
     case 2:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
-      break;
+
     case 3:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
-      break;
+
     case 4:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
-      break;
+
     case 5:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
-      break;
+
     case 6:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
-      break;
+
     case 7:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
-      break;
+
     case 10:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
-      break;
+
     case 11:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
-      break;
+
     case 12:
       return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
-      break;
+
     default:
       return "";
-      break;
   }
 };
 
