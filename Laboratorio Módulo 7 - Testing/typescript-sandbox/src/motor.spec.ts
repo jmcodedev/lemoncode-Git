10c5e0e836210f2ae69018006b5380d7e49d0e11
MSJOAN02 <joan@jmcode.dev>
2024-02-09 20:37:25 +0100

    Testing finalizado

--- Laboratorio Módulo 7 - Testing/typescript-sandbox/src/motor.spec.ts
+++ Laboratorio Módulo 7 - Testing/typescript-sandbox/src/motor.spec.ts
@@ -1,5 +1,6 @@
-import { estadoPartida } from "./motor";
+import { estadoPartida, generarNumeroCarta } from "./motor";
 import { Estado } from "./modelo";
+import { describe, expect, it } from "vitest";
 
 describe("estadoPartida", () => {
   it("Debería devolver HA_GANADO cuando puntos es 7.5", () => {
@@ -9,6 +10,61 @@ describe("estadoPartida", () => {
 
     // Act
     const resultado: Estado = estadoPartida(puntos);
+
+    // Assert
+
+    expect(resultado).toBe(resultadoEsperado);
+  });
+  it("Debería devolver PUEDE_CONTINUAR cuando puntos es menor de 7.5", () => {
+    // Arrange
+    const resultadoEsperado: Estado = "PUEDE_CONTINUAR";
+    const puntos: number = 5;
+
+    // Act
+    const resultado: Estado = estadoPartida(puntos);
+
+    // Assert
+
+    expect(resultado).toBe(resultadoEsperado);
+  });
+  it("Debería devolver GAME_OVER cuando puntos es mayor de 7.5", () => {
+    // Arrange
+    const resultadoEsperado: Estado = "GAME_OVER";
+    const puntos: number = 10;
+
+    // Act
+    const resultado: Estado = estadoPartida(puntos);
+
+    // Assert
+
+    expect(resultado).toBe(resultadoEsperado);
+  });
+});
+
+describe("generarNumeroCarta", () => {
+  it("Debería devolver 10 al introducir 8 por parámetros a la función generarNumeroCarta", () => {
+    // Arrange
+
+    const numeroCarta: number = 8;
+    const resultadoEsperado: number = 10;
+
+    // Act
+
+    const resultado: number = generarNumeroCarta(numeroCarta);
+
+    // Assert
+
+    expect(resultado).toBe(resultadoEsperado);
+  });
+  it("Debería devolver 10 al introducir 9 por parámetros a la función generarNumeroCarta", () => {
+    // Arrange
+
+    const numeroCarta: number = 9;
+    const resultadoEsperado: number = 10;
+
+    // Act
+
+    const resultado: number = generarNumeroCarta(numeroCarta);
 
     // Assert
 
