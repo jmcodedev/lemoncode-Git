f65f9ea218ed4137df0d78ba021776e146b8e585
Joan <joan@jmcode.dev>
2024-05-23 19:46:05 +0200

    .

--- Sandbox-JS/javascript-sandbox/src/main.js
+++ Sandbox-JS/javascript-sandbox/src/main.js
@@ -1,17 +1,40 @@
 import "./style.css";
+import Axios from "axios";
+const __MOVIE_ENDPOINT__ = "http://localhost:3000/movies";
+const __ACTORS_ENDPOINT__ = "http://localhost:3000/actors";
 
-const __CUANTAS_LETRAS = 23;
+const nuevoActor = {
+  name: "Tom Holland",
+  movies: [
+    "Spiderman: Homecoming",
+    "Spiderman: Far from home",
+    "Spiderman: No way home",
+    "Avengers: Infinity War",
+    "Avengers: Endgame",
+    "Captain America: Civil War",
+  ],
+  bio: "Tom Holland is an English actor. He is best known for his role as Spider-Man in the Marvel Cinematic Universe (MCU) films. Holland has also appeared in the disaster film The Impossible (2012), the fantasy drama A Monster Calls (2016), and the war film The Lost City of Z (2016).",
+  image: "https://example.com/tom-holland.jpg",
+};
 
-const dividendo = 23;
+const agregarActor = async () => {
+  const response = await fetch(__ACTORS_ENDPOINT__, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(nuevoActor),
+  });
 
-const resto = dividendo % __CUANTAS_LETRAS;
-
-console.log(
-  `El resto de dividir ${dividendo} entre ${__CUANTAS_LETRAS} es: ${resto}`
-);
-const __LETRAS = "TRWAGMYFPDXBNJZSQVHLCKE";
-
-const letraCalculada = "T";
-
-console.log(letraCalculada === __LETRAS[resto]);
-
+  if (response.ok) {
+    const actor = await response.json();
+    console.log(actor);
+  } else {
+    throw new Error("Error en la llamada a la API");
+  }
+};
+try {
+  agregarActor(nuevoActor);
+} catch (error) {
+  console.log("Error: ", error);
+}
