21459222e2bd0ff012df54772131d4a7e607b496
jmCode <joan@jmcode.dev>
2024-06-07 00:20:12 +0200

    Realizando limpieza

--- Sandbox-TS/src/pelicula-crear/pelicula-crear.api.ts
+++ Sandbox-TS/src/pelicula-crear/pelicula-crear.api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 import { Movie } from "./pelicula-crear.model";
 
-export const crearPelicula = async (pelicula: Movie): Promise<void> => {
-  try {
-    await axios.post("http://localhost:3000/movies", pelicula);
-  } catch (error) {
-    throw new Error(`Error al crear la película`);
-  }
+export const crearPelicula = async (pelicula: Movie): Promise<Movie> => {
+  const URL = "http://localhost:3000/movies";
+  const { data } = await axios.post<Movie>(URL, pelicula);
+
+  return data;
 };
--- Sandbox-TS/src/pelicula-crear/pelicula-crear.ts
+++ Sandbox-TS/src/pelicula-crear/pelicula-crear.ts
@@ -5,17 +5,40 @@ const obtenerValorCampo = (id: string): string => {
   const elementoCampo = document.querySelector(`#${id}`);
 
   if (
-    (elementoCampo && elementoCampo instanceof HTMLInputElement) ||
+    elementoCampo instanceof HTMLInputElement ||
     elementoCampo instanceof HTMLTextAreaElement
   ) {
     return elementoCampo.value;
   } else {
-    throw new Error("Error al obtener el valor del campo");
+    throw new Error(`Error al obtener el campo ${id}`);
   }
 };
 
 const creaPelicula = async (evento: Event): Promise<void> => {
   evento.preventDefault();
-  // TODO: Implementar la creación de la película
-  const pelicula: Movie = {};
+  const pelicula: Movie = {
+    title: obtenerValorCampo("title"),
+    director: obtenerValorCampo("director"),
+    year: Number(obtenerValorCampo("year")),
+    description: obtenerValorCampo("description"),
+    cover_url: obtenerValorCampo("cover_url"),
+  };
+
+  try {
+    await crearPelicula(pelicula);
+    window.location.href = "../pelicula-listado/index.html";
+    alert("Película creada correctamente");
+  } catch (error) {
+    alert(error);
+  }
 };
+
+const iniciarFormulario = () => {
+  const formulario = document.querySelector("#formulario");
+  if (formulario && formulario instanceof HTMLFormElement) {
+    formulario.addEventListener("submit", creaPelicula);
+  } else {
+    throw new Error("No se ha encontrado el formulario");
+  }
+};
+document.addEventListener("DOMContentLoaded", iniciarFormulario);
