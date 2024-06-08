377ce6f9486c4a2f722875dcdf1971124fea7005
jmCode <joan@jmcode.dev>
2024-06-08 10:36:26 +0200

    Lab Async

--- LM 10 - Async/src/personaje.api.ts
+++ LM 10 - Async/src/personaje.api.ts
@@ -4,13 +4,21 @@ import axios from "axios";
 const URL = "http://localhost:3000/personajes";
 
 export const getPersonajes = async (): Promise<Personaje[]> => {
-  const response = await axios.get(URL);
-  return response.data;
+  try {
+    const response = await axios.get(URL);
+    return response.data;
+  } catch (error) {
+    throw new Error("Error al obtener los personajes");
+  }
 };
 
 export const getPersonajePorNombre = async (
   nombre: string
 ): Promise<Personaje[]> => {
-  const response = await axios.get(`${URL}?nombre_like=${nombre}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${URL}?nombre_like=${nombre}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Error al obtener el personaje");
+  }
 };
